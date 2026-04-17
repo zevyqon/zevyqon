18eaf39e41fdc4cab775836305521209525c78eb
ZEVYQON <zevyqon@gmail.com>
2026-04-17 11:39:56 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,9 @@
-
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        Zevyqon is initializing...
+      </h1>
+    </main>
+  );
+}
