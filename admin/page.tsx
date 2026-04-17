83a20d5683071e636d459125bd529d0e467109bd
ZEVYQON <zevyqon@gmail.com>
2026-04-17 21:56:57 +0100

    Delete admin directory

--- admin/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AdminPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-xl">Admin Panel (Coming Soon)</h1>
-    </div>
-  );
-}
