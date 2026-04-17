68b7fd2e4d1615bc0fe2b77412e77a2aa237ac56
ZEVYQON <zevyqon@gmail.com>
2026-04-17 11:42:27 +0100

    Create page.tsx

---
+++ admin/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-xl">Admin Panel (Coming Soon)</h1>
+    </div>
+  );
+}
